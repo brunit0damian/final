dcd909c94b56dbf23491aabc4b5e6c0be5778c28
Bruno <wepbruno@gmail.com>
2022-08-13 23:47:41 -0300

    damos permisos al admin de DJANGO

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Post #importamos el modelo creado Post
 
-# Register your models here.
+admin.site.register(Post) #ahora el modulo admin tiene permiso para crear, modificar y eliminar Post
