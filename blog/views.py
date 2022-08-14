d5084ca08a9fb901fffa2d40c843b9e622296c55
Bruno <wepbruno@gmail.com>
2022-08-13 21:53:17 -0300

    modifique views.py del blog con la funcion mi_vista

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+def mi_vista(request):
+    return HttpResponse("<h1>La pagina de Brunito</h1>")
 
 # Create your views here.
