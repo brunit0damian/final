f744263452272b1ba93233f65f41eecf25a138a1
Bruno <wepbruno@gmail.com>
2022-08-13 22:11:52 -0300

    agregue comentarios a mi views del blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse #importo HttpResponse para que se me haga mas facil mostrar codigo HTML
 def mi_vista(request):
     return HttpResponse("<h1>La pagina de Brunito</h1>")
 
