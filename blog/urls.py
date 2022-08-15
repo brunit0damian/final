dd151e180a8c088287d215416ea30fc397540622
Bruno <wepbruno@gmail.com>
2022-08-15 16:27:21 -0300

    cambio de urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views #importo vistas
 
 #añado patrones de urls enlazados a vistas
 urlpatterns = [
-    path('', views.post_lista, name='post_lista'), #le estoy diciendo que mi '' localhost me muestre la template 'views.post_list'
-    path('a', views.post_lista2, name='post_lista2'),
+    path('a', views.post_lista, name='post_lista'), #le estoy diciendo que mi '' localhost me muestre la template 'views.post_list'
+    path('', views.post_lista2, name='post_lista2'),
 ]
