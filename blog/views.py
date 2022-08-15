6d9124dc672b01efd8b756581c3235074e850714
Bruno <wepbruno@gmail.com>
2022-08-15 15:18:40 -0300

    creo una vista y filtro la base de datos por fecha de publicacion

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,8 @@
+from django.shortcuts import render #importamos metodo para construir plantillas
+from .models import Post #importamos el modelo Post de donde sacamos datos
+from django.utils import timezone #importamos timezone para que nos muestre la hora exacta en caso de un defauld
 
+#funcion de vista
+def post_list(request):                       #QuerySet
+    posts = Post.objects.filter(fecha_de_publicacion__lte=timezone.now()).order_by('fecha_de_publicacion') #sacamos todas las intancias de Post y la ordenamos por su fecha de publicacion y la guardamos en una variable
+    return render(request, 'blog/post_list.html', {'post': posts}) #request es la peticion URL del usuario y entre {} ponemos a mostrar los datos que queremos
