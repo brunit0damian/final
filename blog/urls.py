3ca0bfbd32d817b6b22d953c537b1a5891277976
Bruno <wepbruno@gmail.com>
2022-08-15 15:39:33 -0300

    enlaze la vista con una URL

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views #importo vistas
 #añado patrones de urls enlazados a vistas
 urlpatterns = [
     path('', views.post_lista, name='post_lista'), #le estoy diciendo que mi '' localhost me muestre la template 'views.post_list'
+    path('a', views.post_lista2, name='post_lista2'),
 ]
