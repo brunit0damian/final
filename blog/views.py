36ceb60d1e82a293046891f6658e12b3ee2e12fd
Bruno <wepbruno@gmail.com>
2022-08-15 15:55:15 -0300

    modifique un marcado de html

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,6 @@ from django.utils import timezone #importamos timezone para que nos muestre la h
 #funcion de vista
 def post_lista(request):                       #QuerySet
     posts = Post.objects.filter(fecha_de_publicacion__lte=timezone.now()).order_by('fecha_de_publicacion') #sacamos todas las intancias de Post y la ordenamos por su fecha de publicacion y la guardamos en una variable
-    return render(request, 'blog/template/post_lista.html', {'post': posts}) #request es la peticion URL del usuario y entre {} ponemos a mostrar los datos que queremos    
+    return render(request, 'blog/post_lista.html', {'post': posts}) #request es la peticion URL del usuario y entre {} ponemos a mostrar los datos que queremos    
 def post_lista2(request):
-    return render(request, 'blog/template/post_lista2.html')
+    return render(request, 'blog/post_lista2.html')
