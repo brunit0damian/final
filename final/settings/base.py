98643e92db2eb3b5806caa215849455849747af6
Bruno <wepbruno@gmail.com>
2022-08-15 21:26:17 -0300

    agregue un directorio mas de templates en el base.py del proyecto

--- final/settings/base.py
+++ final/settings/base.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'final.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates")],
+        'DIRS': [os.path.join(BASE_DIR,"templates"),
+        os.path.join(BASE_DIR,"blog.templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
